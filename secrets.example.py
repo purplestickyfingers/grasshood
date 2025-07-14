eb479485912becedc490338e01eafba8734bc6f9
purplestickyfingers <149883596+purplestickyfingers@users.noreply.github.com>
2025-07-14 10:26:02 +0200

    Added Punctuation
    I added a single comma.

--- secrets.example.py
+++ secrets.example.py
@@ -5,4 +5,4 @@ wifi_password = "Wifi Password"
 #Adafruit Credentials
 mqtt_username      = "Adafruit Username"
 mqtt_password      = "Adafruit Active Key"
-mqtt_client_id     = "Anything realy random and unique you don't need to type it anywhere else :)"
+mqtt_client_id     = "Anything realy random and unique, you don't need to type it anywhere else :)"
